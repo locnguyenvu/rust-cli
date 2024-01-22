ba1a236740f310eae80c5f30ed253b6fcbb8db26
locnguyenvu <ngynvuloc@gmail.com>
2024-01-22 10:11:07 +0700

    Finish

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,4 +1,3 @@
-use crate::EntryType::*;
 use std::error::Error;
 use clap::{builder, Command, ArgAction, Arg, ValueEnum, value_parser};
 use regex::Regex;
@@ -26,14 +25,6 @@ impl ValueEnum for EntryType {
     }
 }
 
-impl EntryType {
-    pub fn possible_values() -> impl Iterator<Item = builder::PossibleValue> {
-        Self::value_variants()
-            .iter()
-            .filter_map(ValueEnum::to_possible_value)
-    }
-}
-
 
 #[derive(Debug)]
 pub struct Config {
@@ -50,8 +41,8 @@ pub fn get_args() -> MyResult<Config> {
             Arg::new("paths")
             .action(ArgAction::Append)
             .value_name("PATH")
-            .required(true)
             .default_value(".")
+            .help("Search paths")
         )
         .arg(
             Arg::new("names")
@@ -65,17 +56,63 @@ pub fn get_args() -> MyResult<Config> {
             .short('t')
             .long("type")
             .value_parser(value_parser!(EntryType))
-            .help("Entry type [possible values: f, d, l]")
+            .help("Entry type")
+            .action(ArgAction::Append)
         )
         .get_matches();
 
     let paths: Vec<String> = cmd.get_many::<String>("paths").unwrap().map(|e| e.to_string()).collect();
-    let names: Vec<Regex> = cmd.get_many::<String>("names").unwrap().map(|e| Regex::new(e).unwrap()).collect();
-    let entry_types: Vec<EntryType> = cmd.get_many::<EntryType>("entry_types").unwrap().map(|e| e.to_owned()).collect();
+
+    let matched_names = cmd.get_many::<String>("names");
+    let mut names: Vec<Regex> = vec![];
+    if !matched_names.is_none() {
+        for name in matched_names.unwrap() {
+            match Regex::new(name) {
+                Err(_) => {
+                    std::panic!("Invalid --name \"{}\"", name);
+                }
+                Ok(r) => names.push(r)
+            }
+        }
+    }
+
+    let matched_entry_types = cmd.get_many::<EntryType>("entry_types");
+    let mut entry_types: Vec<EntryType> = vec![];
+    if !matched_entry_types.is_none() {
+        entry_types = matched_entry_types.unwrap().map(|e| e.to_owned()).collect();
+    }
     Ok(Config{ paths, names, entry_types })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    let type_filter = |entry: &walkdir::DirEntry| {
+        config.entry_types.is_empty()
+        || config.entry_types.iter().any(|entry_type| {
+            match entry_type {
+                EntryType::Link => entry.file_type().is_symlink(),
+                EntryType::Dir => entry.file_type().is_dir(),
+                EntryType::File => entry.file_type().is_file()
+            }})
+    };
+    let name_filter = |entry: &walkdir::DirEntry| {
+        config.names.is_empty()
+        || config.names.iter().any(|re| re.is_match(&entry.file_name().to_string_lossy(),))
+    };
+    for path in config.paths {
+        let entries = walkdir::WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e) => {
+                    eprintln!("{}", e);
+                    None
+                }
+                Ok(entry) => Some(entry),
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+            .map(|entry| entry.path().display().to_string())
+            .collect::<Vec<_>>();
+        println!("{}", entries.join("\n"));
+    }
     Ok(())
 }
